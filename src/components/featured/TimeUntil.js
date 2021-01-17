da8729d332c02b5e17f45242eda2fdc4665be05a
genghis.lopez <genghis.lopez@gmail.com>
2021-01-17 14:42:39 -0500

    .

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -4,7 +4,7 @@ import Slide from 'react-reveal/Slide';
 class TimeUntil extends Component {
 
     state = {
-        deadline: 'Dec, 16, 2018',
+        deadline: 'Apr, 1, 2021',
         days:'0',
         hours:'0',
         minutes:'0',
